3e15a81d1275d5398e62736c1a513a8456e0917e
David Hill <davidhill@Davids-Air.fios-router.home>
2016-12-18 00:27:23 -0500

    Tipster 2 with cusom tip feature added

--- Tipster 2/Tipster 2/ViewController.swift
+++ Tipster 2/Tipster 2/ViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  ViewController.swift
 //  Tipster
 //
@@ -16,23 +16,26 @@ class ViewController: UIViewController
     @IBOutlet weak var BillField: UITextField!
     let defaults = UserDefaults.standard
     
+    var CustomTipVal = 0.00
     
-
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func CalculateTip(_ sender: AnyObject)
     {
-        let tipPercentages = [0.05, 0.10, 0.15, 0.20, 0.25]
+        var tipPercentages = [0.05, 0.10, 0.15, 0.20, 0.00]
+        
+        tipPercentages[4] = CustomTipVal
+        
         let bill = Double(BillField.text!) ?? 0
         
         let Tip = bill * tipPercentages[TipCtrl .selectedSegmentIndex]
@@ -42,7 +45,7 @@ class ViewController: UIViewController
         TipLabel.text = String(format: "$%.2f", Tip)
         TotalLabel.text = String(format: "$%.2f", Total)
     }
-        
+    
     @IBAction func OnTap(_ sender: AnyObject)
     {
         view.endEditing(true)
@@ -53,6 +56,14 @@ class ViewController: UIViewController
         print("view will appear")
         // This is a good place to retrieve the default tip percentage from UserDefaults
         // and use it to update the tip amount
+        
+        defaults.synchronize()
+        
+        CustomTipVal = defaults.double(forKey: "nTipVal")
+        
+        print(CustomTipVal)
+        
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -73,7 +84,7 @@ class ViewController: UIViewController
     
     
     
-   
-
+    
+    
 }
 
